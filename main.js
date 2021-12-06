d96c4617f2fdffb6f2b19a429f346d9a0bb2f610
Evelyn-Adams <94699970+Evelyn-Adams@users.noreply.github.com>
2021-12-06 16:47:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-/**  <label>Snapshot - </label>
-  <div id="result"></div>
-  <br><br>
-  <button class="btn btn-success" onclick="check()">Predict Emotion</button> */
+prediction="";
+
+Webcam.set({
+  width:350,
+  height:300,
+  image_format:'png',
+  png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach("#camera")
 
   function take_snapshot(){
       
@@ -9,4 +17,24 @@
     result='<div id="result"></div><br><br>';
     button=' <button class="btn btn-success" onclick="check()">Predict Emotion</button>'
     document.getElementById("div_result").innerHTML=l+result+button;
+
+    
+
+    Webcam.snap(function(data_uri){
+      document.getElementById("div_result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+
+console.log("ml5 version:",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/OHfLoVvc4/model.json",modelLoaded);
+function modelLoaded(){
+  console.log("model loaded");
+}
+
+function speak(){
+  var synth=window.speechSynthesis;
+  speak_data="Prediction is "+prediction;
+  var utterThis=new SpeechSynthesisUtterance(speak_data);
+  synth.speak(utterThis)
 }
